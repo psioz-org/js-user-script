780067407e032b38256041d932ca5a1510148ab9
Zev Zakaryan <nphprog@gmail.com>
2023-04-22 11:17:14 +0700

    chore: update yt ver

--- src/youtube-custom.user.js
+++ src/youtube-custom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Custom
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Move bar and control below the video
 // @author       Zev Zakaryan
 // @match        https://www.youtube.com/*
@@ -10,15 +10,15 @@
 // ==/UserScript==
 
 (function() {
-    'use strict'
-    setTimeout(() => {
-        const x=document.querySelector('.ytp-chrome-bottom')
-        if (!x) return
-        x.style.bottom='-40px'
-        x.style.backgroundColor='green'
-        x.parentNode.parentNode.appendChild(x) //Move so it's visible outside video, delayed otherwise it's removed
-        const title=document.querySelector('#title.ytd-watch-metadata')
-        if (title) title.style.marginTop='40px' //If not delay, style reset
-    },5000)
-    setInterval(() => {const x=document.querySelector('.ytp-ad-skip-button-slot'); if (x) x.click()},1000)
+  'use strict'
+  setTimeout(() => {
+    const x=document.querySelector('.ytp-chrome-bottom')
+    if (!x) return
+    x.style.bottom='-40px'
+    x.style.backgroundColor='green'
+    x.parentNode.parentNode.appendChild(x) //Move so it's visible outside video, delayed otherwise it's removed
+    const title=document.querySelector('#title.ytd-watch-metadata')
+    if (title) title.style.marginTop='40px' //If not delay, style reset
+  },5000)
+  setInterval(() => {const x=document.querySelector('.ytp-ad-skip-button-slot'); if (x) x.click()},1000)
 })()
